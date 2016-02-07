c3486dce05c21a3dfbb20698c66a93e0051cf74b
Dan Cortes <danielgcortes@gmail.com>
2016-02-07 16:50:04 -0600

    Getting started on first page layout

--- _posts/2016-02-06-welcome-to-jekyll.markdown
+++ _posts/2016-02-06-welcome-to-jekyll.markdown
@@ -1,8 +1,8 @@
 ---
 layout: post
-title:  "Welcome to Jekyll!"
-date:   2016-02-06 12:21:51 -0600
-categories: jekyll update
+title:  "Test Blog Post"
+date:   2016-01-01 01:01:01 -0600
+categories: blog
 ---
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
-gulp.task('default', function() {
-  return gulp.src('src/script.js')
+gulp.task('default', ()=> {
+  gulp.src('src/script.js')
     .pipe(babel({
       presets: ['es2015']
       }))
     .pipe(gulp.dest('dist'));
+  gulp.src('_site/css/main.css')
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions']})]))
+    .pipe(gulp.dest('_site/css'));
 });
